14a010698e166e6b69f9a44854777a160e20dba7
Maksym Sevriukov <sevriukovmk@gmail.com>
2023-04-23 22:07:03 +0300

    feat: chapters 3.7.3..3.8.5.3

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,17 @@
 use std::net::TcpListener;
 
-use actix_web::{App, HttpResponse, HttpServer, web};
+use actix_web::{App, HttpServer, web};
 use actix_web::dev::Server;
 
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
+mod routes;
+mod startup;
+pub mod configuration;
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
-            .route("/health_check", web::get().to(health_check))
+            .route("/health_check", web::get().to(routes::health_check))
+            .route("/subscriptions", web::post().to(routes::subscribe))
     })
         .listen(listener)?
         .run();
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::net::TcpListener;
 use zero2prod::run;
+use zero2prod::configuration::get_configuration;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:8080")
+    let config = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", config.app_port);
+    let listener = TcpListener::bind(address)
         .expect("Failed to bind random port.");
     run(listener)?.await
 }

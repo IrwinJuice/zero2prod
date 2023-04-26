6d939a20506c2fd88260fbe92b90cde9c508397d
Maksym Sevriukov <sevriukovmk@gmail.com>
2023-04-26 21:20:30 +0300

    feat: ..3.10

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,3 @@
-use std::net::TcpListener;
-
-use actix_web::dev::Server;
-use actix_web::{web, App, HttpServer};
-
 pub mod configuration;
+pub mod startup;
 mod routes;
-mod startup;
-
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(routes::health_check))
-            .route("/subscriptions", web::post().to(routes::subscribe))
-    })
-    .listen(listener)?
-    .run();
-    Ok(server)
-}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
+use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
-use zero2prod::run;
+use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let config = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&config.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
     let address = format!("127.0.0.1:{}", config.app_port);
     let listener = TcpListener::bind(address).expect("Failed to bind random port.");
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }

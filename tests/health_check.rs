ec5cbe579b4e1b51c12af3cba7dc37ea8dbeef30
Maksym Sevriukov <sevriukovmk@gmail.com>
2023-05-07 21:35:30 +0300

    feat: ..6.7

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -67,7 +67,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     let test_app = spawn_app().await;
     let config = get_configuration().expect("Failed to read configuration.");
     let connection_string = config.database.connection_string();
-    let mut connection = PgConnection::connect(&connection_string)
+    let _connection = PgConnection::connect(&connection_string)
         .await
         .expect("Failed to connect to Postgres.");
     let client = reqwest::Client::new();
@@ -93,7 +93,6 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 #[tokio::test]
 async fn subscribe_returns_a_400_when_data_is_missing() {
     let test_app = spawn_app().await;
-    let config = get_configuration().expect("Failed to read configuration.");
     let client = reqwest::Client::new();
     let test_cases = vec![
         ("name=le%20guin", "missing the email"),
@@ -113,6 +112,33 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
             response.status().as_u16(),
             "The API did not fail with 400 Bad Request when the payload was {}.",
             error_message
+        );
+    }
+}
+
+#[tokio::test]
+async fn subscribe_return_a_200_when_fields_are_present_but_empty() {
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
+        ("name=Ursula&email=", "empty email"),
+        ("name=Ursula&email=definitely-not-an-email", "invalid email"),
+    ];
+
+    for (body, description) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+        assert_eq!(
+            200,
+            response.status().as_u16(),
+            "The API did not fail with 200 OK when the payload was {}.",
+            description
         );
     }
 }
